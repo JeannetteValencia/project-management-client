b3a18017533e2187147fed0236035f10191a4921
jeannette.valencia <jfatima.valencia@gmail.com>
2021-11-11 14:32:30 +0100

    auth : functionality to logout + persist login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import authService from './components/services/auth-services';
 
 import ProjectList from './components/projects/ProjectList';
 import Navbar from './components/navbar/Navbar';
 import ProjectDetails from './components/projects/ProjectDetails';
 import TaskDetails from './components/tasks/TaskDetails';
-
 import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
+
 class App extends Component {
+
+  state = {
+    isLoggedIn: false,
+    user: null
+  };
+
+  getTheUser = (userObj, loggedIn) => {
+    this.setState({
+      isLoggedIn: loggedIn,
+      user: userObj
+    });
+  };
+
+
+  fetchUser = () => {
+    if (this.state.user === null) {
+      authService
+        .loggedin()
+        .then(data => {
+          this.setState({
+            user: data,
+            isLoggedIn: true
+          });
+        })
+        .catch(err => {
+          this.setState({
+            user: null,
+            isLoggedIn: false
+          });
+        });
+    }
+  };
+
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+
+
   render() {
     return (
       <div className="App">
-       <Navbar />
+        <Navbar userData={this.state.user} userIsLoggedIn={this.state.isLoggedIn} getUser={this.getTheUser} />
         <Switch>
-          <Route exact path="/signup" component={Signup}/>
-          <Route exact path="/projects" component={ProjectList}/>
+          <Route exact path="/" render={props => <Login {...props} getUser={this.getTheUser} />} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/projects" component={ProjectList} />
           <Route exact path="/projects/:id" component={ProjectDetails} />
           <Route exact path="/projects/:id/tasks/:taskId" component={TaskDetails} />
         </Switch>
