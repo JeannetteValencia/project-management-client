56687db4a3340cb08ef9f134273a217e13433f35
jeannette.valencia <jfatima.valencia@gmail.com>
2021-11-17 11:45:37 +0100

    Protected Routes

---
+++ src/components/auth/ProtectedRoute.js
@@ -0,0 +1,16 @@
+// auth/ProtectedRoute.js
+
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const ProtectedRoute = ({ component: Component, user, ...rest }) => {
+  console.log({ component: Component, user, rest });
+
+  if (user) {
+    return <Route {...rest} render={routeProps => <Component {...routeProps} userData={user} />} />;
+  } else {
+    return <Redirect to={{ pathname: '/', state: { from: rest.location } }} />;
+  }
+};
+
+export default ProtectedRoute;
